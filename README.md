c2896b44cbede2814117c8d3877ea9c6e305dd1d
Zach Bjornson <bjornson@stanford.edu>
2015-09-01 20:02:29 -0700

    Updates after testing

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Python script for incrementally backing up CyTOF data to Google Cloud Storage.
 
 Compared to the three earlier versions, this version is simpler and relies more on gsutil's rsync-like functionality. It does not provide versioning like the previous scripts because that was essentially unused. If a file is modified after being backed up initially, the new version will replace whatever was previously backed up.
 
-- Compresses IMD files (only) and deletes the uncompressed IMD immediately after compression. **Caution: All tested versions of the CyTOF software will abort acquisition if *any* IMD is deleted while acquisition is in process.**
+- Compresses IMD files (only) and deletes the original IMD immediately after compression. Files that are still being written to by the CyTOF software will be skipped, but... *Caution: The CyTOF software may abort acquisition if any IMD file is deleted during acquisition.* (This happens on some occasions, but not all.)
 - Displays a message after it's done syncing with the date that the sync was started.
 
 Install instructions:
 
 1. Setup a project in the Google Developers Console.
-1. Install Python 2.6.x or 2.7.x.
+1. Install Python 2.7.x.
 1. Download gsutil from [here](https://storage.googleapis.com/pub/gsutil.zip) and extract to C:\.
 1. Setup access by running `python C:\gsutil\gsutil config` and following the prompts. When prompted for the project name, enter the name of the project from step 1.
 1. Install 7zip (e.g. this: http://www.7-zip.org/a/7z1506-x64.exe). Note the install location (typically $env:ProgramFiles\7-Zip\7z.exe).
--- backup.py
+++ backup.py
@@ -4,27 +4,33 @@ from subprocess import call
 import glob
 import ctypes
 import datetime
+import fnmatch
 
-sz = r"C:/Program Failes/7-Zip/7z.exe"
-bucket_name = "firedragon"
-local_data_path = "F:/data/" # with trailing slash!
-log_path = "F:/logs/"
+sz = r"C:\\Program Files\\7-Zip\\7z.exe"
+gsutil = r"C:\\gsutil\\gsutil"
+bucket_name = "janis_joplin"
+local_data_path = r"E:\\" # with trailing slash!
 
 # Scan local_data_path for .imd files
-imd_files = glob.glob("{0}*.imd".format(local_data_path))
+imd_files = []
+for root, dirs, filenames in os.walk(local_data_path, topdown=True):
+  dirs[:] = [d for d in dirs if d not in set(["$RECYCLE.BIN"])]
+  for filename in fnmatch.filter(filenames, "*.imd"):
+    imd_files.append(os.path.join(root, filename))
 
 # 7z each imd file
 for f in imd_files:
-    return_value = call([sz, "a", "{0}.7z".format(f), f])
-    if return_value != 0:
-        raise Exception("Failed zipping {0}".format(f))
-    else:
-        os.remove(f)
+  return_value = call([sz, "a", "{0}.7z".format(f), f])
+  if return_value != 0:
+    print("Skipped zipping {0}, perhaps because it is being written to by the CyTOF software".format(f))
+    os.remove("{0}.7z".format(f)) # remove the empty archive
+  else:
+    os.remove(f)
 
 # Run rsync, excluding .imd files
 start_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-return_value = call(["gsutil", "-m", "rsync", "-x '.*\.imd'", "-r", local_data_path, "gs://{0}".format(bucket_name)])
+return_value = call([gsutil, "-m", "rsync", "-x '.*\.imd'", "-rn", local_data_path, "gs://{0}".format(bucket_name)])
 if return_value != 0:
-    raise Exception("rsync exited with code {0}".format(return_value))
+  raise Exception("rsync exited with code {0}".format(return_value))
 else:
-    ctypes.windll.user32.MessageBoxA(0, "Backups through {0} complete.".format(start_date), "CyTOF backups", 0)
+  ctypes.windll.user32.MessageBoxA(0, "Backups through {0} complete.".format(start_date), "CyTOF backups", 0)
